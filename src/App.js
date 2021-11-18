4e2da34dae31a443850dbe57e617708236446e8e
vishnugopy <vishnugoby0010@gmail.com>
2021-11-19 00:06:08 +0100

    theme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import './App.scss';
-import CV from './components/CvSection';
-
+import "./App.scss";
+import CV from "./components/CvSection";
 
 function App() {
   return (
     <main className="main">
-      <div className="container"></div> 
       <CV />
-
     </main>
   );
 }
