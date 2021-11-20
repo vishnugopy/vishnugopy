6ef694e3ba992476f72757752a5b98dc7c520e81
vishnugopy <vishnugoby0010@gmail.com>
2021-11-20 22:52:42 +0100

    redesign

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.scss";
+
+import Header from "./components/Header";
 import CV from "./components/CvSection";
 import Hero from "./components/Hero";
 
 function App() {
   return (
     <main className="main">
+      <Header />
       <Hero />
     </main>
   );
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import "./style.scss";
+
+function Header() {
+  return (
+   <header>
+       WEBSITE UNDER CONSTRUCTION
+   </header>
+  );
+}
+
+export default Header;
