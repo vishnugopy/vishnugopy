434604718faf7679a8f7681a1d094fbeaab90df5
vishnugopy <vishnugoby0010@gmail.com>
2021-11-28 00:28:52 +0100

    vishnugopy.dev

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 Je suis un développeur javascript et j'apprends des trucs pour m'améliorer. 😋
 
-Vous pouvez également visiter ma page en cliquant ( vishnugopy.dev )
+Vous pouvez également visiter ma page en cliquant
+https://vishnugopy.dev/
--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import "./App.scss";
 
-import Header from "./components/Header";
 import CV from "./components/CvSection";
-import Hero from "./components/Hero";
 
 function App() {
-  return (
-
-      <CV />
-
-
-  );
+  return <CV />;
 }
 
 export default App;
