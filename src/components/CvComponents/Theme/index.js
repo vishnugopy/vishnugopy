7c5f0704acf0caa5313a9b1a867e2d944e954490
vishnugopy <vishnugoby0010@gmail.com>
2021-11-28 21:47:53 +0100

    vishnugopy.dev

--- src/components/CvComponents/Theme/index.js
+++ src/components/CvComponents/Theme/index.js
@@ -4,20 +4,17 @@ import Brightness4RoundedIcon from "@mui/icons-material/Brightness4Rounded";
 
 const Theme = () => {
   useEffect(() => {
+    
     const toggle = document.getElementById("darktheme");
+
     toggle.addEventListener("click", () => {
-      document.documentElement.classList.add("darkhtml");
-      localStorage.setItem("darktheme", "true");
+      document.documentElement.classList.toggle("darkhtml");
     });
 
-    if (localStorage.getItem("darktheme") === "true") {
-      document.documentElement.classList.add("darkhtml");
-      toggle.addEventListener("click", () => {
-        document.documentElement.classList.remove("darkhtml");
-        localStorage.setItem("darktheme", "false");
-      });
-    }
+   
   }, []);
+
+
 
   return (
     <div id="darktheme" className="theme">
