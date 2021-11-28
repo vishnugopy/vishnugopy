9def14383a03c644122502f8452971acdac82edd
vishnugopy <vishnugoby0010@gmail.com>
2021-11-28 12:45:51 +0100

    vishnugopy.dev

--- src/components/CvComponents/Theme/index.js
+++ src/components/CvComponents/Theme/index.js
@@ -5,17 +5,25 @@ import Brightness4RoundedIcon from "@mui/icons-material/Brightness4Rounded";
 const Theme = () => {
   useEffect(() => {
     const toggle = document.getElementById("darktheme");
-
     toggle.addEventListener("click", () => {
-      document.documentElement.classList.toggle("darkhtml");
+      document.documentElement.classList.add("darkhtml");
+      localStorage.setItem("darktheme", "true");
     });
+
+    if (localStorage.getItem("darktheme") === "true") {
+      document.documentElement.classList.add("darkhtml");
+      toggle.addEventListener("click", () => {
+        document.documentElement.classList.remove("darkhtml");
+        localStorage.setItem("darktheme", "false");
+      });
+    }
   }, []);
 
   return (
-      <div id="darktheme" className="theme">
-        <Brightness4RoundedIcon />
-        <p>Theme</p>
-      </div>
+    <div id="darktheme" className="theme">
+      <Brightness4RoundedIcon />
+      <p>Theme</p>
+    </div>
   );
 };
 
