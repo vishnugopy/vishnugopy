e7a84a98a3b63beaf770e77f2c42e28ec1bdd43f
vishnugopy.suthaharan <vishnugopy.suthaharan@o4software.com>
2022-01-09 14:15:03 +0100

    redesign

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -7,6 +7,11 @@ function PlayGround() {
     <section className="home">
     hellooooo i know no one care but my site is on construction
 
+    <h1>Vishnugopy</h1>
+    <h2>Développeur web et web mobile</h2>
+    <h2>UI Designer</h2>
+    <h2>Auto Entreprenuer</h2>
+
     </section>
   );
 }
