7f8d2966f2a9fb607df97b8561970f3e638a74f5
vishnugopy.suthaharan <vishnugopy.suthaharan@o4software.com>
2022-01-09 21:14:54 +0100

    redesign in prograss

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,17 +1,45 @@
 import "./style.scss";
-
+import {useEffect} from "react";
 
 function PlayGround() {
-  
+
+  function randomHsl() {
+    return 'hsla(' + (Math.random() * 360) + ', 100%, 50%, 1)';
+}
+
+    useEffect(() => {
+        const nameElement = document.querySelectorAll(".my-name h1");
+
+        nameElement.forEach(element => {
+          element.addEventListener("mouseover", () => {
+            element.style.color = randomHsl();
+          })
+        });
+    }, []);
+
+
   return (
     <section className="home">
-    hellooooo i know no one care but my site is on construction
-
-    <h1>Vishnugopy</h1>
-    <h2>Développeur web et web mobile</h2>
-    <h2>UI Designer</h2>
-    <h2>Auto Entreprenuer</h2>
-
+      <main>
+        <div className="hero-section">
+          <div className="my-name">
+            <h1>V</h1>
+            <h1>I</h1>
+            <h1>S</h1>
+            <h1>H</h1>
+            <h1>N</h1>
+            <h1>U</h1>
+            <h1>G</h1>
+            <h1>O</h1>
+            <h1>P</h1>
+            <h1>Y</h1>
+            </div>
+          <div className="positions">
+            <span className="my-position">Développeur web</span>
+            <span className="my-position">Graphiste</span>
+          </div>
+        </div>
+      </main>
     </section>
   );
 }
