fe4bc33dfc64a86dc053a5c1398cfe6ded807964
vishnugopy <vishnugoby0010@gmail.com>
2022-09-08 11:00:58 +0200

    improvements

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import PlayGround from "./pages/PlayGround";
 
-
-
 import Header from "./components/Header";
 
 function App() {
