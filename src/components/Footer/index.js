5c076e8a7a249c918f27a5241e9e49c5016e897f
vishnugopy <vishnugoby0010@gmail.com>
2023-08-23 16:59:44 +0200

    link to privacy

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,11 @@
+import { Link } from "react-router-dom";
 import "./style.scss";
 
 function Footer() {
   return (
     <section className="footer">
-      <p>
-      Ce site est réalisé avec ❤️  par Vishnu Gopy
-      </p>
+      <p>Ce site est réalisé avec ❤️ par Vishnu Gopy</p>
+      <Link to="/privacy">Politique de confidentialité</Link>
     </section>
   );
 }
