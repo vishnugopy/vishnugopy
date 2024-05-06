b0c52679d8aa2f7789207948fc45fb5ab11d131b
vishnugopy <vishnugoby0010@gmail.com>
2024-05-06 16:29:30 +0200

    chore: Add font-awesome library for signature page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
+import Signature from "./pages/Signature";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/sign" element={<Signature />} />
       </Routes>
     </Router>
   );
